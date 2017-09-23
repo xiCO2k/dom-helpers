f145b7d0425795aa3dd414030329cbd2b93729d8
Francisco Madeira <xico2k@gmail.com>
2017-09-23 01:13:09 +0100

    changes

--- index.js
+++ index.js
@@ -4,3 +4,4 @@ export { ignoreSelection } from './src/ignoreSelection';
 export { isChildOf } from './src/isChildOf';
 export { position } from './src/position';
 export { scrollTo } from './src/scrollTo';
+export default { getHeight, getWidth, ignoreSelection, isChildOf, position, scrollTo };
